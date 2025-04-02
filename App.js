9e6349c6fce41be441fb8731e05adc028a08509a
SamRobledo <148101246+SamRobledo@users.noreply.github.com>
2025-04-01 23:17:45 -0600

    Registro v1

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Moniplaga test</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet } from 'react-native';
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Toaster } from 'sonner-native';
+import InicioSesion from './screens/InicioSesion';
+import Pantalla_Principal from "./screens/Pantalla_Principal";
+import RecuperarContraseña from './screens/Recuperar Contraseña';
+import Registro from './screens/Registro';
+  
+  const Stack = createNativeStackNavigator();
+  
+  function RootStack() {
+    return (
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+          animation: 'fade',
+          contentStyle: { backgroundColor: '#2A5B3E' } 
+        }}
+      >
+    
+        <Stack.Screen name="InicioSesion" component={InicioSesion} />
+        <Stack.Screen name="Registro" component={Registro} />
+        <Stack.Screen name="Recuperar" component={RecuperarContraseña} />
+        <Stack.Screen name="Home" component={Pantalla_Principal} />
+        
+        
+        
+      </Stack.Navigator>
+    );
+  }
